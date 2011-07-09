7007d26b904c6d299e1f85ed89897b1d1c1921e9
Alberto Aram Rascon <aram123@gmail.com>
2011-07-09 17:51:42 -0600

    Inicio del proyecto closesis

---
+++ data.php
@@ -0,0 +1,9 @@
+<?php 
+$DatabaseType = 'mysql'; 
+$DatabaseServer = 'localhost'; 
+$DatabaseUsername = 'root'; 
+$DatabasePassword = 'hambre'; 
+$DatabaseName = 'opensis'; 
+$DatabasePort = '3306'; 
+$DefaultSyear = '2010'; 
+?>
